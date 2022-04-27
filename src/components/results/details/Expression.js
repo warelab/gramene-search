d9596b276747999341f5b1eb740ae194a1e7a5a8
Andrew Olson <olson@cshl.edu>
2022-04-27 14:27:16 -0400

    allow non-root paths

--- src/components/results/details/Expression.js
+++ src/components/results/details/Expression.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {connect} from "redux-bundler-react";
 
+const PREFIX = (global.location ? global.location.origin + global.location.pathname : '');
+
 const Detail = props => {
   const gene = props.geneDocs[props.searchResult.id];
-  const url = `/static/atlasWidget.html?${gene.taxon_id === 112509 ? gene.synonyms[0] : gene._id}`;
+  const url = `${PREFIX}static/atlasWidget.html?${gene.taxon_id === 112509 ? gene.synonyms[0] : gene._id}`;
   const height = '500px';
   return (
     <iframe src={url} frameBorder="0" width="100%" height={height}>
