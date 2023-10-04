40a56b24df3eb205a0863344f0ae773fc51a2679
Andrew Olson <olson@cshl.edu>
2023-10-04 11:14:18 -0400

    bar bundle if we need it

--- src/bundles/BAR.js
+++ src/bundles/BAR.js
@@ -1,58 +1,27 @@
 import {createSelector} from "redux-bundler";
-const grameneGenomes = {
-  name: 'grameneGenomes',
+const BARs = {
+  name: 'BARs',
   getReducer: () => {
     const initialState = {
-      show: false,
-      active: {}
+
     };
     return (state = initialState, {type, payload}) => {
       let newState;
       switch (type) {
-        case 'GRAMENE_GENOMES_TOGGLED':
-          newState = Object.assign({},state);
-          newState.show = !newState.show;
-          return newState;
-        case 'GRAMENE_GENOMES_UPDATED':
-          newState = Object.assign({}, state);
-          newState.active = payload;
-          return newState;
+        // case 'GRAMENE_GENOMES_TOGGLED':
+        //   newState = Object.assign({},state);
+        //   newState.show = !newState.show;
+        //   return newState;
+        // case 'GRAMENE_GENOMES_UPDATED':
+        //   newState = Object.assign({}, state);
+        //   newState.active = payload;
+        //   return newState;
         default:
           return state;
       }
     }
   },
-  doShowGrameneGenomes: () => ({dispatch, getState}) => {
-    dispatch({type: 'GRAMENE_GENOMES_TOGGLED'})
-  },
-  doUpdateGrameneGenomes: genomes => ({dispatch, getState}) => {
-    dispatch({type: 'BATCH_ACTIONS', actions: [
-        {type: 'GRAMENE_SEARCH_CLEARED'},
-        {type: 'GRAMENE_GENOMES_TOGGLED'},
-        {type: 'GRAMENE_GENOMES_UPDATED', payload: genomes}]})
-  },
-  doEnsureGrameneGenome: genome => ({dispatch, getState}) => {
-    const state = getState();
-    if (Object.keys(state.grameneGenomes.active).length > 0 && !state.grameneGenomes.active.hasOwnProperty(genome)) {
-      let active = Object.assign({}, state.grameneGenomes.active);
-      active[genome]=true;
-      dispatch({type: 'GRAMENE_GENOMES_UPDATED', payload: active})
-    }
-  },
-  selectGrameneGenomes: state => state.grameneGenomes,
-  selectActiveGenomes: state => {
-    return Object.keys(state.grameneGenomes.active)
-  },
-  selectActiveGenomeCount: createSelector(
-    'selectGrameneMaps',
-    'selectGrameneGenomes',
-    (maps,genomes) => {
-      if (maps && genomes) {
-        return Object.keys(genomes.active).length || Object.keys(maps).length;
-      }
-      return 0;
-    }
-  )
+  selectBARs: state => state.BARs,
 };
 
-export default grameneGenomes;
+export default BARs;
