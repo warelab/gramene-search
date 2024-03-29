aa6667208c50635171caa76e8ccd15a8934e08b1
Andrew Olson <olson@cshl.edu>
2024-03-29 09:47:45 -0400

    SorbMutDB link

--- src/components/results/details/VEP.js
+++ src/components/results/details/VEP.js
@@ -5,13 +5,30 @@ import * as console from "console";
 import { AgGridReact } from "ag-grid-react";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
+import "./VEP.css";
 
 const metaRenderer = params => {
   if (params.value.field === "germplasm") { // link to grin or SorgMutDB
     if (params.value.germplasm_dbid) {
       return <a target="_blank" href={`https://npgsweb.ars-grin.gov/gringlobal/accessiondetail.aspx?id=${params.value.germplasm_dbid}`}>{params.value.pub_id}</a>;
     }
-    return `link to SorgMutDB ${params.value.pub_id}`
+    return (
+      <form id={params.value.pub_id} action="https://www.depts.ttu.edu/igcast/sorbmutdb.php" method="post" target="_blank">
+        <input type="hidden" name="search" value={params.value.gene_id.replace('SORBI_3','Sobic.')} />
+        <input type="hidden" name="submit" value="Search" />
+        <button type="submit" className="button-like-link">SorbMutDB</button>
+      </form>
+    );
+
+    return (
+      <form id={params.value.pub_id} action="https://www.depts.ttu.edu/igcast/sorbmutdb.php" method="post" target="_blank">
+        <input type="hidden" name="search" value={params.value.gene_id.replace('SORBI_3','Sobic.')} />
+        <input type="hidden" name="submit" value="Search" />
+        <button className="button-like-link" onClick={() => document.getElementById(params.value.pub_id).submit()}>SorbMutDB</button>
+      </form>
+    );
+
+    return `link to SorbMutDB ${params.value.pub_id}`
   }
   if (params.value.field === "search") { // search filter
     return `VEP__merged__${study_info[params.value.pop_id].type}__attr_ss:${params.value.ens_id}`
@@ -53,7 +70,7 @@ const Detail = props => {
               'Study/Population': {label: study_info[parts[3]].label},
               'VEP consequence': {label: parts[1].replaceAll("_"," ")},
               'Allele Status': {label: parts[2] === "het" ? "heterozygous" : "homozygous"},
-              'Germplasm': {field: 'germplasm', label: germplasm.pub_id, ...germplasm},
+              'Germplasm': {field: 'germplasm', gene_id: props.searchResult.id, label: germplasm.pub_id, ...germplasm},
               'Other Genes': {field: 'search', label: germplasm.ens_id, ...germplasm}
             };
             accessionTable.push(accInfo);
