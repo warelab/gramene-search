ea5007068b235ee63c536a3fd5b6a24db1ee8cfd
Andrew Olson <olson@cshl.edu>
2024-04-17 12:35:41 -0400

    reorder expression tabs and auto resize atlas widget iframe

--- src/components/results/details/Expression.js
+++ src/components/results/details/Expression.js
@@ -1,7 +1,63 @@
-import React, { useState, useEffect } from 'react'
+import React, { useRef, useEffect } from 'react'
 import {connect} from "redux-bundler-react";
 import {Tabs, Tab} from 'react-bootstrap';
 import BAR, {haveBAR} from "gramene-efp-browser";
+
+function DynamicIframe(props) {
+  // Create a ref for the iframe element
+  const iframeRef = useRef(null);
+
+  // Function to resize iframe height
+  const resizeIframe = () => {
+    if (iframeRef.current) {
+      const iframe = iframeRef.current;
+      const innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+      iframe.style.height = 44 + innerDoc.body.scrollHeight + 'px';
+    }
+  };
+
+  // Resize iframe when content loads
+  useEffect(() => {
+    resizeIframe();
+  }, []); // Empty dependency array ensures it only runs once after initial render
+
+  // Optional: Resize iframe when window is resized
+  useEffect(() => {
+    window.addEventListener('resize', resizeIframe);
+    return () => {
+      window.removeEventListener('resize', resizeIframe);
+    };
+  }, []); // Empty dependency array ensures it only runs once after initial render
+
+  // Resize iframe when content changes
+  useEffect(() => {
+    const iframe = iframeRef.current;
+    if (!iframe) return;
+
+    const observer = new MutationObserver(resizeIframe);
+    const checkElement = () => {
+      const innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+      const targetElement = innerDoc.querySelector('#heatmapContainer');
+      if (targetElement) {
+        observer.observe(targetElement, { attributes: true, childList: true, subtree: true });
+      } else {
+        setTimeout(checkElement, 200); // Check again after 100 milliseconds
+      }
+    };
+    checkElement();
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <iframe
+      ref={iframeRef}
+      src={props.url}
+      title="Dynamic Iframe"
+      style={{ width: '100%', border: 'none' }}
+    />
+  );
+}
 
 const Detail = props => {
   const gene = props.geneDocs[props.searchResult.id];
@@ -18,13 +74,15 @@ const Detail = props => {
     props.doRequestParalogExpression(gene._id)
   }
   return <Tabs>
-    {haveBAR(gene) && <Tab tabClassName="eFP" eventKey="eFP" title="eFP Browser"><BAR gene={gene}/></Tab>}
-    <Tab tabClassName="gxa" eventKey="gene" title="All Studies">
-      <iframe src={gene_url} frameBorder="0" width="100%" height="500px"></iframe>
-    </Tab>
-    {paralogs_url && <Tab tabClassName="gxa" eventKey="paralogs" title="Reference Study (all paralogs)">
-      <iframe src={paralogs_url} frameBorder="0" width="100%" height="500px"></iframe>
-    </Tab>}
+    {paralogs_url &&
+      <Tab tabClassName="gxa" eventKey="paralogs" title="Reference Study (all paralogs)">
+        <DynamicIframe url={paralogs_url}/>
+      </Tab>
+    }
+    <Tab tabClassName="gxa" eventKey="gene" title="All Studies"><DynamicIframe url={gene_url}/></Tab>
+    {haveBAR(gene) &&
+      <Tab tabClassName="eFP" eventKey="eFP" title="eFP Browser"><BAR gene={gene}/></Tab>
+    }
   </Tabs>
 };
 
